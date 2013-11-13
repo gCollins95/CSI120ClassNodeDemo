9e4c16a2b30a392e437b97ffcf596f94275b1a04
OpenShift guest <gcollins192@gmail.com>
2013-11-12 20:15:21 -0500

    procfile

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+  var output = {
+      "name": "Guthrie Collins",
+      "vacation" : {"place": ["44.59, -72.35", "40.010, -75.259", "18.220, -66.59"]}
+    };
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send(output);
 });
 
 var port = process.env.PORT || 5000;
